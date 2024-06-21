2b1b3ef8dd20c05356df37e840c4c5b587827d39
minhbguy <104999039+minhbguy@users.noreply.github.com>
2024-06-21 17:41:53 +0700

    s

--- persona-web/index.js
+++ persona-web/index.js
@@ -131,3 +131,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', checkVisibility);
 });
+
+
+const themeButton = doc.getElementById('theme-mode');
+
+themeButton.addEventListener ('click', () => {
+    doc.body.classList.toggle('dark-mode');
+    if(doc.body.classList.contains('dark-mode')){
+        themeButton.classList.remove('fa-moon-o');
+        themeButton.classList.add('fa-sun-o');
+    }
+    else{
+        themeButton.classList.remove('fa-sun-o');
+        themeButton.classList.add('fa-moon-o');
+    }
+})
